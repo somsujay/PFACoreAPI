afef418dd74bb6427a92bfa2ef182bf30bca2058
sujaysom <som.sujay@gmail.com>
2024-10-23 20:31:49 -0400

    2nd Commit

--- app/db/database.py
+++ app/db/database.py
@@ -1,21 +1,6 @@
 import aiomysql
 from app.util.bootstrap.boot_util import bootstrapper  # Import the loaded settings
 
-# async def get_db_pool():
-#     pool = await aiomysql.create_pool(
-#         host=bootstrapper.db_host,       # Use the config values from YAML
-#         port=bootstrapper.db_port,
-#         user=bootstrapper.db_user,
-#         password=bootstrapper.db_password,
-#         db=bootstrapper.db_name,
-#         autocommit=True
-#     )
-#     return pool
-
-# app/db/database.py
-# import aiomysql
-# from fastapi import FastAPI
-
 DATABASE_CONFIG = {
     "host": bootstrapper.db_host,
     "port": bootstrapper.db_port,
