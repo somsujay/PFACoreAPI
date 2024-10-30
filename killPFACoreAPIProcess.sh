16224d6b1e694ac2b0a8dc0318888c57d66196c6
somsujay <36706686+somsujay@users.noreply.github.com>
2024-10-29 21:11:34 -0400

    BatchScript for running & Killing (#3)

--- killPFACoreAPIProcess.sh
+++ killPFACoreAPIProcess.sh
@@ -11,16 +11,16 @@ pid=$(lsof -t -i:8000)
 
 # Check if the process ID exists
 if [ -n "$pid" ]; then
-  echo "Killing process with PID $pid running on port 8050..."
+  echo "Killing process with PID $pid running on port 8000..."
   kill -9 $pid
   echo "Process killed."
 else
   echo "No process found running on port 8000."
 fi
 
-source ~/PFAEnvs/py311API/bin/activate
-
-cd ./PortFolioAnalytics/PFACoreAPI/
-
-uvicorn app.main:app --port 8000 --reload
+#source ~/PFAEnvs/py311API/bin/activate
+#
+#cd ./PortFolioAnalytics/PFACoreAPI/
+#
+#uvicorn app.main:app --port 8000 --reload
 
